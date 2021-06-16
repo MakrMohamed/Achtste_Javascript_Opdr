b37ac130fc115959d3235b3c08d33aef74729fe9
MakrMohamed <84314376+MakrMohamed@users.noreply.github.com>
2021-06-16 13:15:51 +0200

    Add files via upload
    8e Js Opdr - return

---
+++ script.js
@@ -0,0 +1,46 @@
+/*const bigNumber = function (number) {
+    const check = (number < 100 && number > 100);
+    if (number <= 100) {
+        return false;
+    };
+    if (number > 100) {
+        return true;
+    };
+};
+
+const result = bigNumber(3);
+console.log(result);
+//This function produces something
+*/
+
+/*const Brenda = function (maxNumb, currNumb, age) {
+    const check = (maxNumb == 500 && currNumb == 400 && age >= 18);
+
+    if (age < 18) {
+        return "this is a club for adults";
+    };
+    if (currNumb >= maxNumb) {
+        return "it's too busy now, come back later";
+    };
+    if (currNumb < maxNumb) {
+        return "come in";
+    };
+
+};
+const result = Brenda.age (25);
+console.log(result);
+//This function does something 
+*/
+
+
+
+const calculateAverage = function (numbers) {
+    const average = (grades / grades.length);
+    return average;
+
+};
+
+const grades = [6, 7, 8, 9, 5];
+console.log(average);
+calculateAverage(grades);
+//This function produces something
